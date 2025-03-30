041c1b25cd3d87eb303608fbb927dd070e4d985f
Ahsan Khan <mohammadahsan2010@gmail.com>
2025-03-30 04:10:59 +0400

    updated wsgi

--- automation/views.py
+++ automation/views.py
@@ -13,23 +13,23 @@ def automation_view(request):
         action = request.POST.get("action")
 
         if action == "provision":
-            process = Popen(["bash", "provision.sh"], cwd="/home/ubuntu/automation/terraform-openstack-ec2", stdout=PIPE, stderr=PIPE)
+            process = Popen(["bash", "provision.sh"], cwd="/home/ubuntu/openstack-casestudy/terraform-openstack-ec2", stdout=PIPE, stderr=PIPE)
             stdout, stderr = process.communicate()
             terraform_output = stdout.decode() + stderr.decode()
 
         elif action == "destroy":
-            process = Popen(["bash", "destroy.sh"], cwd="/home/ubuntu/automation/terraform-openstack-ec2", stdout=PIPE, stderr=PIPE)
+            process = Popen(["bash", "destroy.sh"], cwd="/home/ubuntu/openstack-casestudy/terraform-openstack-ec2", stdout=PIPE, stderr=PIPE)
             stdout, stderr = process.communicate()
             destroy_output = stdout.decode() + stderr.decode()
 
         elif action == "run_ansible":
-            process = Popen(["ansible-playbook", "playbook.yaml"], cwd="/home/ubuntu/automation/ansible", stdout=PIPE, stderr=PIPE)
+            process = Popen(["ansible-playbook", "playbook.yaml"], cwd="/home/ubuntu/openstack-casestudy/ansible", stdout=PIPE, stderr=PIPE)
             stdout, stderr = process.communicate()
             ansible_output = stdout.decode() + stderr.decode()
 
         # Optional: simple EC2 listing (could be part of ansible output)
         try:
-            list_proc = Popen(["ansible", "all", "-m", "ping"], cwd="/home/ubuntu/automation/ansible", stdout=PIPE, stderr=PIPE)
+            list_proc = Popen(["ansible", "all", "-m", "ping"], cwd="/home/ubuntu/openstack-casestudy/ansible", stdout=PIPE, stderr=PIPE)
             out, err = list_proc.communicate()
             ec2_instances_output = out.decode() + err.decode()
         except Exception as e:
