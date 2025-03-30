ef5f580484958d2d70c86eacf989ba8bccd1de94
Ahsan Khan <mohammadahsan2010@gmail.com>
2025-03-30 05:32:06 +0400

    allowd to create resources now

--- terraform-openstack-ec2/provision.sh
+++ terraform-openstack-ec2/provision.sh
@@ -6,8 +6,8 @@ DESTROY_STATUS="/home/ubuntu/openstack-casestudy/automation/logs/status/destroy_
 export PATH=$PATH:/usr/bin:/usr/local/bin
 
 /usr/bin/terraform init
-/usr/bin/terraform plan -var-file="terraform.tfvars"
-#/usr/bin/terraform apply -var-file="terraform.tfvars" -auto-approve
+#/usr/bin/terraform plan -var-file="terraform.tfvars"
+/usr/bin/terraform apply -var-file="terraform.tfvars" -auto-approve
 
 echo "provisioned" > "$STATUS_FILE"
 echo "available to destroy" > "$DESTROY_STATUS"
